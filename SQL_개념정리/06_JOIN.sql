f3e9680fa236bf13ff4c64cb587098a72c70202c
sieunn <73224848+sieunn@users.noreply.github.com>
2024-06-04 17:24:11 +0900

    subquery

--- SQL_개념정리/06_JOIN.sql
+++ SQL_개념정리/06_JOIN.sql
@@ -98,3 +98,187 @@ FROM DEPARTMENT, LOCATION
 WHERE LOCATION_ID = LOCAL_CODE;
 
 
+
+/*
+연결에 사용할 두 컬럼이 다른 경우 --> JOIN ON 
+
+연결에 사용할 두 컬럼이 같은 경우 --> JOIN USING
+
+--2) 연결에 사용할 두 컬럼이 같은 경우 --> JOIN USING을 사용
+FROM 테이블명
+JOIN 테이블명 USING(같은컬럼명)
+*/
+-- EPMPOYEE 테이블, JOB테이블 참조해서
+-- 사번 이름 직급코드 직급명 조회
+
+-- ANSI 표준 연결방법
+-- 연결에 사용할 컬럼이 같은 경우 USING(컬럼명) 사용
+SELECT EMP_ID, EMP_NAME, JOB_CODE 
+FROM EMPLOYEE;
+/*
+EMP_ID, EMP_NAME, JOB_CODE
+200	    선동일	    J1
+201	    송종기	    J2
+202	    노옹철	    J2
+203	    송은희	    J4
+204	    유재식	    J3
+205	    정중하	    J3
+206	    박나라	    J7
+207	    하이유	    J5
+208	    김해술	    J5
+209	    심봉선	    J3
+.
+.
+*/
+
+SELECT * FROM JOB;
+/*
+JOB_CODE, JOB_NAME
+J1	        대표
+J2	        부사장
+J3	        부장
+J4	        차장
+J5	        과장
+J6	        대리
+J7	        사원
+*/
+
+SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+FROM EMPLOYEE
+JOIN JOB USING (JOB_CODE);
+/*
+ANSI 표준 규격으로 작성해서 나온 조회결과
+EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+200	    선동일	    J1	    대표
+201	    송종기	    J2	    부사장
+202	    노옹철	    J2	    부사장
+203	    송은희	    J4	    차장
+204	    유재식	    J3	    부장
+.
+.
+*/
+
+SELECT E.EMP_ID, E.EMP_NAME, E.JOB_CODE, J.JOB_NAME
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE = J.JOB_CODE;
+/*
+ORACLE 표준 규격으로 작성해서 나온 조회결과
+EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+200	    선동일	    J1	    대표
+201	    송종기	    J2	    부사장
+202	    노옹철	    J2	    부사장
+203	    송은희	    J4	    차장
+204	    유재식	    J3	    부장
+.
+.
+*/
+
+
+/*****NATURAL JOIN(자연조인) 
+
+동일한 타입과 이름을 가진 컬럼이 있는 테이블 간의 조인을 간단히 표현한 방법
+반드시 두 테이블 간에 동일한 컬럼명, 타입을 가진 컬럼이 필요
+없을 경우 교차조인이 됨
+
+EMPLOYEE.JOB_CODE (CHAR(2))
+     JOB.JOB_CODE (CHAR(2))
+     -->컬럼명, 자료형이 모두 일치해야 자연조인 대상
+
+SELECT 컬럼명, 컬럼명, 컬럼명,...
+FROM 테이블명 
+NATURAL JOIN 테이블명
+
+JOIN에 별칭을 설정해서 테이블 표현하기
+JOIN과 FROM에서 테이블을 설정할 때 
+쿼리를 좀 더 간결하고 읽기 쉽게 만드는 방법
+테이블을 다수 여러개 참조할 때 유용ㅇ
+
+사용방법
+1) SELECT 테이블명.컬럼명, 테이블명.컬럼명
+   FROM 테이블명, 테이블명
+   WHERE 테이블명.컬럼명 = 테이블명.컬럼명
+   
+2) SELCET A.컬럼명 = B.컬럼명
+   FROM 테이블명 A, 테이블명 B
+   WHERE A.컬럼명 = B.컬럼명
+
+******/
+
+
+
+-- ******  OUTER JOIN 외부조인 ******
+-- 두 테이블의 지정하는 컬럼값이 일치하지 않는 행도
+-- 조인에 포함 시킴
+
+-- OUTER JOIN 과 비교할 INNER JOIN 쿼리문
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE
+/*INNER*/JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+
+-- 1) LEFT [OUTER] JOIN
+-- 합치기에 사용한 두 테이블 중에서 왼쪽 편에 작성된 테이블의
+-- 컬럼수를 기준으로 JOIN
+
+-- ANSI 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+--> JOIN 구문 기준으로 왼쪽에 작성된 테이블의 
+--> 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 JOIN 
+
+-- ORACLE 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT 
+WHERE DEPT_CODE = DEPT_ID(+);
+--> DEPT_CODE와 DEPT_ID값이 일치하지 않아도
+-- 왼쪽에 있는 컬럼수에 맞게 표시를 넣어라 (NULL값이라도 넣어라)
+
+
+-- 2) RIGHT [OUTER] JOIN
+-- 합치기에 사용한 두 테이블 중에서 오른쪽 편에 작성된 테이블의
+-- 컬럼수를 기준으로 JOIN
+
+-- ANSI 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE RIGHT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+--> JOIN 구문 기준으로 오른쪽에 작성된 테이블의 
+--> 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 JOIN 
+
+-- ORACLE 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT 
+WHERE DEPT_CODE(+) = DEPT_ID;
+--> DEPT_CODE와 DEPT_ID값이 일치하지 않아도
+--오른쪽에 있는 컬럼수에 맞게 표시를 넣어라 (NULL값이라도 넣어라)
+
+
+-- 3) FULL [OUTER] JOIN
+-- 합치기에 사용한 두 테이블이 가진 모든 행을 결과에 포함
+--> 오라클에서는 FULL OUTER JOIN 구문 사용불가
+
+-- ANSI 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE FULL JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+--> JOIN 구문 기준으로 양쪽에 작성된 테이블의 
+--> 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 JOIN 
+
+-- ORACLE 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT 
+WHERE DEPT_CODE(+) = DEPT_ID(+);
+/*
+ORA-01468: a predicate may reference only one outer-joined table
+01468. 00000 -  "a predicate may reference only one outer-joined table"
+*Cause:    
+*Action:
+268행, 20열에서 오류 발생
+*/
+
+
+
+
+
+
+
+
+
